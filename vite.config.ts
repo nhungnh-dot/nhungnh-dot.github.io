778c5106d7b387fa1ffec70a8319052390f088e7
nhungnh-dot <nhungnh@ksu.edu>
2026-09-03 14:28:56 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
-
-export default defineConfig({
-  base: '/nhungnguyen/',
-  plugins: [react(), tailwindcss()],
-  // existing settings...
-});
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
     base: '/nhungnguyen/',
-    plugins: [react(), tailwindcss()],
+
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
 
     resolve: {
       alias: {
@@ -26,5 +24,3 @@ export default defineConfig(() => {
     },
   };
 });
-
-
